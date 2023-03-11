e150e083d67e06db8fe6d82c9b3f6f67c681851e
Lutfur Rahman Sarker <eng.lrsarker@gmail.com>
2023-03-11 22:36:43 +0600

    Update simpleCalculatorPython.py

--- simpleCalculatorPython.py
+++ simpleCalculatorPython.py
@@ -1,8 +1,8 @@
 # Make a Simple Calculator using Python if statement!
 
-x = int(input("Enter 1st Number: "))
-z = input("Enter Oparetor: (+, -, *, /, %) ")
-y = int(input("Enter 2nd Number: "))
+x = int(input('Enter 1st Number: '))
+z = input('Enter Oparetor: (+, -, *, /, %) ')
+y = int(input('Enter 2nd Number: '))
 
 if z == '+':
     print('Total', x + y)
@@ -15,4 +15,4 @@ elif z == '/':
 elif z == '%':
     print('Total', x % y)
 else:
-    print("Operator Missing")
+    print('OOPS :( Something is Wrong! Please try again ')
